601b11910751c10aae1d73cbf29f88e3f4dc8f1e
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-12-18 13:30:36 +0700

    rev

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss','@nuxt/http'],
-  http: {
-    baseURL: 'http://localhost:3000/wp-json/',  // URL API WordPress lokal
-    headers: {
-      common: {
-        'Access-Control-Allow-Origin': '*', // Mengizinkan permintaan dari domain manapun
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-WP-Nonce',
-      },
-    },
-  },
+  modules: ['@nuxtjs/tailwindcss'],
+
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || '',
