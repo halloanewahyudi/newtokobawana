e852f71998951f54bc70c76914edc4280cb3a040
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-12-18 11:18:39 +0700

    commit lagi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  http: {
+    headers: {
+      common: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-WP-Nonce',
+      },
+    },
+  },
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || '',
